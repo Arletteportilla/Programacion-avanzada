12f5f6dad52b07e7911a57c600bd5bdd1a9a4a18
Arlette Portilla <arportillaca@uide.edu.ec>
2022-10-28 08:48:41 -0500

    Primer commit

---
+++ Fundamentos/ejemplo.py
@@ -0,0 +1,16 @@
+def suma ():
+    return 2+2
+
+print (suma())
+ 
+def resta ():
+    return 2-2
+print (resta()) 
+
+def multiplicacion():
+    return 2*3
+print(multiplicacion())
+
+def division():
+    return 6/3
+print(division())
